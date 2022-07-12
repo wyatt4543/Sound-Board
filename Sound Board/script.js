6cabf7f13554e3bb098f86c4fcd79f39ef06b2c4
wyatt4543 <93059435+wyatt4543@users.noreply.github.com>
2022-07-11 21:40:42 -0500

    Add files via upload
    im reuploading good files

---
+++ Sound Board/script.js
@@ -0,0 +1,25 @@
+const sounds = ['yellow', 'ben', 'yes', 'no', 'hohoho', 'bleh', 'bouncy ball boing', 'death:minecraft', 'oof:roblox', 'goose honk', 'rick roll', 'ay ay like a little butterfly', 'you violated the law', 'yahoo:mario', 'webee wah woop:crazy dave:pvz', 'a lot of damage', 'gullible on the ceiling', 'Clash of Clans startup', 'smack AH', 'vine boom', 'Baba Booey']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    
+    btn.innerText = sound
+    
+    btn.addEventListener('click', () => {
+        stopSongs()
+        
+        document.getElementById(sound).play()
+    })
+    
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}

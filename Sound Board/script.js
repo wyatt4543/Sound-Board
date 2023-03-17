b1f4268b9bfc73ec77f9094548a49ff83d2a675b
wyatt4543 <9yearsoldwhenstarted@gmail.com>
2023-03-17 00:22:53 -0500

    added open na noor sound

--- Sound Board/script.js
+++ Sound Board/script.js
@@ -1,4 +1,4 @@
-const sounds = ['yellow', 'ben', 'yes', 'no', 'hohoho', 'bleh', 'bouncy ball boing', 'death:minecraft', 'oof:roblox', 'goose honk', 'rick roll', 'ay ay like a little butterfly', 'you violated the law', 'yahoo:mario', 'webee wah woop:crazy dave:pvz', 'a lot of damage', 'gullible on the ceiling', 'Clash of Clans startup', 'smack AH', 'vine boom', 'Baba Booey']
+const sounds = ['yellow', 'ben', 'yes', 'no', 'hohoho', 'bleh', 'bouncy ball boing', 'death:minecraft', 'oof:roblox', 'goose honk', 'rick roll', 'ay ay like a little butterfly', 'you violated the law', 'yahoo:mario', 'webee wah woop:crazy dave:pvz', 'a lot of damage', 'gullible on the ceiling', 'Clash of Clans startup', 'smack AH', 'vine boom', 'Baba Booey', 'Open Na Noor']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
